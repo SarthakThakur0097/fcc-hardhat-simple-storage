97fbc127d841404e3382399a9f4a3bf96c490b06
Sarthak <sarthakthakur0097@gmail.com>
2022-07-25 00:53:57 -0400

    ENV file changed

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ require("hardhat-gas-reporter")
 require("solidity-coverage")
 
 /** @type import('hardhat/config').HardhatUserConfig */
-const RINKEBY_RPC_URL = process.env.RPC_URL
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 module.exports = {
